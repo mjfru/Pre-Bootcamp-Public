94c439462fd2a8d97c13a702ff65c07dd18e5833
Matt Frutchey <108436172+mjfru@users.noreply.github.com>
2023-03-26 12:20:29 -0400

    Add files via upload

---
+++ Frutchey_Matt_LoopingThrough.js
@@ -0,0 +1,27 @@
+// 1. Write a for loop that will traverse through an array of numbers, and print each number.
+/*var arr1 = [8, 6, 7, 5, 3, 0, 9];
+for (let i = 0; i < arr1.length; i++){
+    console.log(arr1[i])
+} */
+
+// 2. Write a for loop that will traverse through an array of numbers, and print the sum of the number and the index of the number in the array.
+/* let sum = 0
+var arr2 = [4, 7, 13, 13, 19, 37, -2];
+for (i = 0; i < arr2.length; i++){
+    sum = sum + arr2[i]
+    console.log(`The sum is ${sum}.`)
+    console.log(`The index in the array is ${i}.`)
+} */
+
+// 3. Write a for loop that will traverse through an array of numbers, and print ONLY the numbers greater than 5.
+/* var arr3 = [6, 2, 12, 14, -24, 5, 0];
+for (let i = 0; i < arr3.length; i++){
+    if (arr3[i] > 5){
+        console.log(arr3[i])
+    } else {
+        arr3[i] = "No dice."
+        // console.log(arr3)
+    }
+} */
+
+// NINJA BONUS: Modify your solution for #3 so that any numbers in the array that are NOT greater than 5 are instead replaced with a string of "No dice." This string should NOT be printed.
---
+++ Frutchey_Matt_Parameters.js
@@ -0,0 +1,10 @@
+function greetPerson(time, name){
+    if (name == "Count Dooku"){
+        console.log("I'm coming for you, Dooku!")
+    }
+    else {
+    console.log(`Good ${time}, ${name}.`)
+    }
+}
+greetPerson("evening", "Count Dooku")
+
